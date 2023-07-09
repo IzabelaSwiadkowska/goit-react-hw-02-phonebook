476b60405ace960f7cb49d33f7e3cdfd3fb9c2a5
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-07-09 22:04:10 +0200

    hmw 2 -phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import Filter from './Filter/Filter';
+import styles from './ContactForm/ContactForm.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.state.contacts.some(
+      el =>
+        el.name.toLowerCase().trim() === contact.name.toLowerCase().trim() ||
+        el.number.trim() === contact.number.trim()
+    )
+      ? alert(`${name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, contact],
+        }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  addFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+  filteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const inputContact = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(inputContact)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <div className={styles.wrapper}>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter filter={filter} addFilter={this.addFilter} />
+        <ContactList
+          contacts={this.filteredContacts()}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
