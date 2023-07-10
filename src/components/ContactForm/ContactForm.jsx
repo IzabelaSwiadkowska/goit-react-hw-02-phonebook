90eb06faf0c77d2e3c94ddd8a068470aa487cacf
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-07-10 11:03:08 +0200

    pattern name corrected

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,6 +27,8 @@ class ContactForm extends Component {
     this.setState({ ...INITIAL_STATE });
   };
 
+  nameRegex = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
+
   render() {
     const { name, number } = this.state;
     return (
@@ -43,7 +45,7 @@ class ContactForm extends Component {
             onChange={this.handleChange}
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
